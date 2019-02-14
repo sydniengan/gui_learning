3e472d576d8ea2e7f9bc43f550458ba5e83bed9e
nganuridgesy <nganuridgesy@wgc.school.nz>
2019-02-15 09:59:19 +1300

    Added a label

--- helloworld.py
+++ helloworld.py
@@ -1,7 +1,12 @@
 import tkinter
+
 root = tkinter.Tk()
 
-root.title("Hello World!!!")	# sets the title of the window
-root.geometry("600x200")    	# sets the window's width and height
+root.title("Hello World!!!")  # sets the title of the window
+root.geometry("600x200")  # sets the window's width and height
+# create a label
+my_label = tkinter.Label(root)  # creates a label and attaches it to the window
+my_label.config(text="Hello!", fg="green")  # options to configure the label
+my_label.grid()  # a special method for placing the widget on the window
 
 root.mainloop()
