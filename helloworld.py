e217b11445f1644dc4a43882666d662ad00a8c2c
nganuridgesy <nganuridgesy@wgc.school.nz>
2019-02-15 09:58:50 +1300

    Added title and size for window

--- helloworld.py
+++ helloworld.py
@@ -1,4 +1,7 @@
 import tkinter
 root = tkinter.Tk()
 
+root.title("Hello World!!!")	# sets the title of the window
+root.geometry("600x200")    	# sets the window's width and height
+
 root.mainloop()
